076ce52bb116a5c080f735cbd09927bba3a2a0b5
Misley-tech <77426773+Misley-tech@users.noreply.github.com>
2021-10-31 22:38:20 -0300

    Update cod_2.py

--- cod_2.py
+++ cod_2.py
@@ -3,7 +3,7 @@ import soundfile as sf
 from scipy import signal
 import matplotlib.pyplot as plt
 
-"""             ######## Refencias ########
+               ######## Refencias ########
 #cal: senal de calibración senoidal de 1 kHz a 1 Pa (94 dBSPL).
 #ruido: senal de ruido de fondo.
 #rms: valor eficaz de la senal de calibracion.
@@ -11,10 +11,9 @@ import matplotlib.pyplot as plt
 #Leq: vector que almacena el Leq de cada banda.
 #leq: valor que almacena el valor de una banda.
                 ###########################
-"""
-
+                
 #importo la senal de calibracion y de ruido de fondo
-cal, fs = sf.read('calibración_prueba.wav') #importo senal de calibracion
+cal, fs = sf.read('calibracion_prueba.wav') #importo senal de calibracion
 ruido, fs = sf.read('ruido_fondo_prueba.wav') #importo ruido de fondo
 
 
@@ -26,53 +25,84 @@ rms = np.sqrt(sum(cal**2)/len(cal))
 ruido = ruido/rms
 
 #3) Filtro la senal ruido por tercio de octava.
-# Estas son las frecuencias centrales normalizadas que figuran en la normativa
-# ANSIS XXX de cada banda de tercio de octava
+# Estas son las frecuencias centrales normalizadas que 
+# figuran en la normativa ANSI S1.11-1986 por cada 
+# banda de tercio de octava
 
 f0 = [25, 31.5, 40, 50, 63, 80, 100, 125, 160, 200, 250, 315, 400, 500, 630, 
           800, 1000, 1250, 1600, 2000, 2500, 3150, 4000, 5000, 6300, 8000,
           10000, 12500, 16000]
 
-N = np.arange(14,43,1) #Leer pag. 5 de ANSI
-K = N - 30
-fr = 1000
-fm = fr*(2**(K/3))
 
 # para calcular las frecuencias superior e inferior hay que aplicar
-# f1 = 2^(1/6) * fcentral
-# f2 = 2^(-1/6) * fcentral
-# para filtrarlo se utiliza un butterworth pasabanda de 6to orden, indicado
-# por la normativa.
+# f1 = 2^(1/6) * fm
+# f2 = 2^(1/6) * fm
 
-Leq=[] 
+b = 1/3 
+N = np.arange(14,44,1) 
+K = N - 30 
+fr = 1000  #la frecuencia de referencia es de 1 kHz para audio
 
-for i in range(0,len(f0)):
-    Wn = ((f0[i]*(2**(-1/6)/(0.5*fs))),f0[i]*(2**(1/6)/(0.5*fs)))
-    sos = signal.butter(6,Wn,btype='bandpass',output='sos',analog=True)
-    a,b = signal.butter(6,Wn,btype='bandpass',output='ba',analog=True)
-    w, h = signal.freqs(a,b)
-    #plt.semilogx(w, 20*np.log10(abs(h)))
+fmax = []
+fmin = []
+Fm = []
+
+for i in range(len(N)):
+    fm = fr*(2**(K[i]*b)) # ¿Por qué?
+    Fm.append(fm)
+    f1 = (fm*(2**(-b/2)))/(0.5*fs)
+    fmin.append(f1)
+    f2 = (fm*(2**(b/2)))/(0.5*fs)
+    fmax.append(f2)
+    
+print(fmin)
+print(fmax)
+print(Fm)
+
+
+# El tipo de filtro que se indica en la normativa es un 
+# butterworth pasabanda de 6to orden. Luego se calcula 
+# el valor rms de la senal por banda y finalmente se 
+# expresa este valor en dB SPL
+
+Wn = [fmin,fmax]
+SPL = []
+
+for i in range(0,len(N)):
+    sos = signal.butter(6,(Wn[0][i],Wn[1][i]),btype='bandpass',output='sos')
+    a,b = signal.butter(6,(Wn[0][i],Wn[1][i]),btype='bandpass',analog=True,output='ba')
+    w, h = signal.freqs(a,b) 
+    plt.semilogx(w*fs/2, 20*np.log10(abs(h))) #¿Por qué?
     ruido_fil = signal.sosfilt(sos, ruido)
     
     rms_banda=np.sqrt(sum(ruido_fil**2)/len(ruido_fil))
-    leq = 20*np.log10(rms_banda/20e-6)
-    print(rms_banda)
-    Leq.append(leq)
-    
+    spl = 20*np.log10(rms_banda/20e-6)
+    SPL.append(spl)
 
-print(Leq)
-    
-    
-    
-#4) Ploteo del gráfico de barras
+# hago un vector de strings de las frecuencias 
+# centrales normalizadas para el ploteo.
+xlabels = ['25', '31.5', '40', '50', '63', '80', '100', '125', '160', '200', '250', '315', '400', '500', '630', 
+          '800', '1000', '1250', '1600', '2000', '2500', '3150', '4000', '5000', '6300', '8000',
+          '10000', '12500','16000']
 
-xlabels = ['25' ,'31.5', '40','50','63','80','100','125','160','200','250',
-           '315','400','500','630','800','1k','1.25k','1.6k','2k','2.5k',
-           '3.15k','4k','5k','6.3k','8k','10k','12.5k','16k']
-
-plt.bar(f0,Leq)
+plt.xticks(f0, xlabels, rotation=70)
+plt.xlim(20,20000)
+plt.ylim(-10, 2.5)
+plt.xlabel(r'$Frecuencia\ (Hz)$', fontsize=12)
+plt.ylabel(r'$Magnitud\ (dB)$', fontsize=13)
+plt.grid()
 plt.tight_layout()
-# plt.xlim(20,20000)
-# plt.ylim(-20, 2.5)
-# plt.xlabel(r'$Frecuencia\ [Hz]$', fontsize=12)
-# plt.ylabel(r'$Magnitud\ [dB]$', fontsize=13)
+plt.savefig('rta_filtros.png')    
+
+# genero un vector de igual largo que el de
+# las bandas normalizadas
+freqs = np.arange(0,len(N)-1,1)
+
+plt.figure()
+plt.bar(range(len(Fm)),SPL)
+plt.xticks(freqs,xlabels,rotation=70)
+plt.ylabel(r'$L_{eq} \ (dBSPL)$', fontsize=13)
+plt.xlabel(r'$Frecuencia \ (Hz)$', fontsize=12)
+plt.tight_layout()
+plt.savefig('ruido_tercios.png')
+plt.show()
