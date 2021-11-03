860fb707602cf9c48f1a9d8de3fd840b642f6771
Uriel Lansky <uriellansky@gmail.com>
2021-11-02 21:15:29 -0300

    No anda gráfico

---
+++ Flaiban_Zelener_contorno.py
@@ -0,0 +1,83 @@
+import pandas as pd
+from matplotlib import pyplot as plt
+import numpy as np
+import scipy as sc
+
+
+# %%
+
+def contorno(fs, t_ref, archivo, eje='h', elemento=''):
+    if eje == 'v':
+        orientacion = 'vertical'
+    elif eje == 'h':
+        orientacion = 'horizontal'
+
+    lim_sup = round(fs * t_ref + 4)
+    grados = np.linspace(0, 180, 37, dtype=int)
+    df = pd.read_csv(archivo)
+    h = df[grados][4:lim_sup]
+    h = np.array(h)
+    h = np.transpose(h)
+
+    ############ FFT #############
+
+    v_fft = np.hstack([h, np.zeros((37, fs - lim_sup))])
+    v_fft = sc.fft.rfft(v_fft)
+    v_fft_log = 20 * np.log10(abs(v_fft))
+    matriz_polar = v_fft_log - v_fft_log[0, :]
+
+    fig, ax = plt.subplots(subplot_kw={'projection': 'polar'})
+    theta = np.pi * np.hstack((-1 * np.flip(grados), grados)) / 180
+    freqs = [1000, 2000, 4000, 8000, 12000, 16000]
+    labels = ['1 kHz', '2 kHz', '4 kHz', '8 kHz', '12 kHz', '16 kHz']
+
+    for i, f in enumerate(freqs):
+        x = np.hstack([np.flip(matriz_polar[:, f]),
+                       matriz_polar[:, f]])
+        ax.plot(theta, x, label=labels[i])
+
+    ax.set_rlim((-55, 0))
+    ax.set_rticks(np.arange(-50, 0, 10))
+    ax.set_rlabel_position(0)
+    ax.tick_params(axis='x', which='both', labelsize=10)
+    ax.legend(bbox_to_anchor=(1.05, 1), loc='best')
+    ax.set_title(f'Patrón polar {orientacion} ({elemento})', fontsize=13,
+                 pad=15)
+    plt.savefig(f'polar_{eje}_{elemento}.png')
+    plt.show()
+    plt.tight_layout()
+
+    ########## CONTORNO ##################
+
+    matriz_contorno = np.vstack((np.flip(np.delete(matriz_polar, 0, 0), axis=0),
+                                 matriz_polar))
+
+    plt.figure()
+    contorno_y = np.linspace(-180, 180, 73, dtype=int)
+    contorno_x = np.linspace(0, fs / 2, len(matriz_contorno[0, :]))
+    levels = [-33, -30, -27, -24, -21, -18, -15, -12, -9, -6, -3, 0, 3, 6]
+    X, Y = np.meshgrid(contorno_x, contorno_y)
+    plt.contour(X, Y, matriz_contorno, levels=[-6])
+    plt.contourf(X, Y, matriz_contorno, levels, cmap='jet', extend='min')
+    plt.gca().set_xscale('log')
+    ticksx = [500, 1000, 2000, 5000, 10000, 20000]
+    ticksy = [-180, -90, 0, 90, 180]
+    plt.xticks(ticksx, ticksx)
+    plt.xlim(500, 20000)
+    plt.yticks(ticksy, ticksy)
+    cbar = plt.colorbar(drawedges='True')
+    cbar.set_label('Nivel relativo [dB]', fontsize=12)
+    plt.title(f'Gráfico de contorno {orientacion} ({elemento})', fontsize=13)
+    plt.xlabel('Frecuencia [Hz]', fontsize=12)
+    plt.ylabel('Ángulo [°]', fontsize=12)
+    plt.savefig(f'contorno_{eje}_{elemento}.png')
+    plt.show()
+
+
+fs = 48000
+t_ref = 0.0108
+archivo_H = 'Bocina_ME20_0_180_H.xlsx'
+archivo_V = 'Bocina_ME20_0_180_V.xlsx'
+
+contorno(fs, t_ref, archivo_H, eje='h', elemento='ME20')
+contorno(fs, t_ref, archivo_V, eje='v', elemento='ME20')
--- cod_1.py
+++ cod_1.py
@@ -4,18 +4,24 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
+# Funciones
+
+
 def matriz_impulsos(orientacion="H"):
     """
     Esta función obtiene los distintos impulsos de los archivos .csv y convierte en un vector "tiempo" y una "impulsos"
     :param orientacion: Para eje horizontal, "H" o "h". Para eje vertical, "V" o "v".
     Eje horizontal como predeterminado.
-    :return: Devuelve un vector "tiempo" para graficar como eje horizontal y una matriz "impulsos" con los
-    valores a distintas direcciones (con pasos de 5°).
+    :return: Devuelve un vector "tiempo" para graficar como eje horizontal, una matriz "impulsos" con los
+    valores a distintas direcciones (con pasos de 5°), un vector con el eje frecuencial de la fft y una
+    matriz de fft.
     """
 
     # Definición de variables
     array_conteo = np.linspace(0, 180, num=37, dtype=int)  # Vector de ángulos
     impulsos = np.array(np.zeros(73), dtype=object)  # Vector vacío para impulsos
+    nuevo_imp = np.zeros(73)
+    imp_fft = np.array(np.zeros(73), dtype=object)  # Vector vacío para impulsos pasados por fft
     tiempo = np.array([0])  # Vector vacío para tiempo
     for i in range(len(array_conteo)):  # Elige eje horizontal o vertical. Por defecto, horizontal
         if orientacion == "H" or orientacion == "h":
@@ -30,36 +36,56 @@ def matriz_impulsos(orientacion="H"):
         archivo2 = archivo2.astype(float)
         archivo3 = archivo2[294:489, 1]  # Ventaneo eligiendo sólo las muestras que quiero
         # (desde 6.125 ms durante 4.063 ms)
-        if (i == 0):
+        if i == 0:
             tiempo = archivo2[294:489, 0]  # Definición vector tiempo con mismo ventaneo
             impulsos[36] = archivo3  # Definición matriz "impulsos" para ángulo 0
+            imp_fft[36] = 20 * np.log10(abs(np.fft.rfft(archivo3)))
         else:
             impulsos[i + 36] = archivo3  # Definición matriz "impulsos" para ángulos positivos
             impulsos[36 - i] = archivo3  # Definición matriz "impulsos" para ángulos negativos
-        del (archivo)
-        del (archivo2)
-        del (archivo3)
-    return tiempo, impulsos
-
-def fft(matriz):
-    mat_fft = np.fft.rfft(matriz)
-    # for i in range(len(matriz)):
-    #     mat_fft[i] = np.fft.rfft(matriz[i])
-    matriz_fft = 20*np.log10(mat_fft)
-    freq = np.linspace(0, 24000, len(mat_fft))
-    return freq, matriz_fft
+            imp_fft[i+36] = 20 * np.log10(abs(np.fft.rfft(archivo3)))
+            imp_fft[36-i] = 20 * np.log10(abs(np.fft.rfft(archivo3)))
+        del archivo
+        del archivo2
+        del archivo3
+    freq = np.linspace(0, 24000, len(imp_fft[0]))
+    tiempo = tiempo*1000
+    # imp_fft[36] = imp_fft[36]-imp_fft[36]
+    return tiempo, impulsos, freq, imp_fft
 
 
-tiempo, impulsos = matriz_impulsos()
+def grafico(t, mat_t, f, mat_f):
 
-frec, vector = fft(impulsos[0])
+    contorno_x = f
+    contorno_y = np.linspace(-180, 180, 73, dtype=int)
+    print(type(contorno_y))
+    print(type(contorno_x))
+    levels = [-33, -30, -27, -24, -21, -18, -15, -12, -9, -6, -3, 0, 3, 6]
+    X, Y = np.meshgrid(contorno_x, contorno_y)
+    plt.contourf(X, Y, mat_f, levels, cmap='jet', extend='min')
+
+    # plt.figure(1)
+    # plt.subplot(121)
+    # plt.plot(t, mat_t[35])
+    # plt.subplot(122)
+    # plt.semilogx(f, mat_f[35])
+    #
+    # plt.figure(2)
+    # plt.subplot(121)
+    # plt.plot(t, mat_t[36])
+    # plt.subplot(122)
+    # plt.semilogx(f, mat_f[36])
+    #
+    # plt.figure(3)
+    # plt.subplot(121)
+    # plt.plot(t, mat_t[37])
+    # plt.subplot(122)
+    # plt.semilogx(f, mat_f[37])
+    plt.show()
 
 
-# Gráfico
-plt.plot(frec, vector)
-plt.show()
+tiempo, impulsos, frecuencia, imp_fft = matriz_impulsos()
+print(type(imp_fft))
 
-"""
-NO FUNCIONA FFT
-En otra función hacer rfft y normalizar al valor en 0°. Hacer función gráfico.
-"""
+grafico(tiempo, impulsos, frecuencia, imp_fft)
+
