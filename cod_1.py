2172e411535f03ad9b7d0964dd9af24db7495204
Uriel Lansky <uriellansky@gmail.com>
2021-11-03 00:14:37 -0300

    Basta por hoy

--- cod_1.py
+++ cod_1.py
@@ -2,6 +2,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import suavizado as su
 
 
 # Funciones
@@ -19,10 +20,10 @@ def matriz_impulsos(orientacion="H"):
 
     # Definición de variables
     array_conteo = np.linspace(0, 180, num=37, dtype=int)  # Vector de ángulos
-    impulsos = np.array(np.zeros(73), dtype=object)  # Vector vacío para impulsos
-    nuevo_imp = np.zeros(73)
-    imp_fft = np.array(np.zeros(73), dtype=object)  # Vector vacío para impulsos pasados por fft
+    impulsos = np.array(np.zeros([73, 195]), dtype=object)  # Vector vacío para impulsos
+    imp_fft = np.array(np.zeros([73, 513]), dtype=object)  # Vector vacío para impulsos pasados por fft
     tiempo = np.array([0])  # Vector vacío para tiempo
+    N = 1024
     for i in range(len(array_conteo)):  # Elige eje horizontal o vertical. Por defecto, horizontal
         if orientacion == "H" or orientacion == "h":
             archivo = pd.read_csv("Archivos/de620me90_hor_deg{:.0f}.csv".format(array_conteo[i]), header=None)
@@ -34,17 +35,20 @@ def matriz_impulsos(orientacion="H"):
         archivo = archivo.drop([0, 1, 2])  # Elimina las primeras 3 filas que sólo tienen encabezados
         archivo2 = archivo.to_numpy()
         archivo2 = archivo2.astype(float)
-        archivo3 = archivo2[294:489, 1]  # Ventaneo eligiendo sólo las muestras que quiero
+        archivo3 = archivo2[293:488, 1]  # Ventaneo eligiendo sólo las muestras que quiero
         # (desde 6.125 ms durante 4.063 ms)
         if i == 0:
             tiempo = archivo2[294:489, 0]  # Definición vector tiempo con mismo ventaneo
             impulsos[36] = archivo3  # Definición matriz "impulsos" para ángulo 0
-            imp_fft[36] = 20 * np.log10(abs(np.fft.rfft(archivo3)))
+            imp_fft_ceros = np.hstack([archivo3, np.zeros(415)])
+            imp_fft[36] = 20 * np.log10(abs(np.fft.rfft(imp_fft_ceros, N)))
+
         else:
             impulsos[i + 36] = archivo3  # Definición matriz "impulsos" para ángulos positivos
             impulsos[36 - i] = archivo3  # Definición matriz "impulsos" para ángulos negativos
-            imp_fft[i+36] = 20 * np.log10(abs(np.fft.rfft(archivo3)))
-            imp_fft[36-i] = 20 * np.log10(abs(np.fft.rfft(archivo3)))
+            imp_fft_ceros = np.hstack([archivo3, np.zeros(415)])
+            imp_fft[i + 36] = 20 * np.log10(abs(np.fft.rfft(imp_fft_ceros, N)))#-imp_fft[36]
+            imp_fft[36 - i] = 20 * np.log10(abs(np.fft.rfft(imp_fft_ceros, N)))#-imp_fft[36]
         del archivo
         del archivo2
         del archivo3
@@ -54,38 +58,35 @@ def matriz_impulsos(orientacion="H"):
     return tiempo, impulsos, freq, imp_fft
 
 
-def grafico(t, mat_t, f, mat_f):
+def grafico(f, mat_f):
 
     contorno_x = f
     contorno_y = np.linspace(-180, 180, 73, dtype=int)
-    print(type(contorno_y))
-    print(type(contorno_x))
     levels = [-33, -30, -27, -24, -21, -18, -15, -12, -9, -6, -3, 0, 3, 6]
-    X, Y = np.meshgrid(contorno_x, contorno_y)
-    plt.contourf(X, Y, mat_f, levels, cmap='jet', extend='min')
-
-    # plt.figure(1)
-    # plt.subplot(121)
-    # plt.plot(t, mat_t[35])
-    # plt.subplot(122)
-    # plt.semilogx(f, mat_f[35])
-    #
-    # plt.figure(2)
-    # plt.subplot(121)
-    # plt.plot(t, mat_t[36])
-    # plt.subplot(122)
-    # plt.semilogx(f, mat_f[36])
-    #
-    # plt.figure(3)
-    # plt.subplot(121)
-    # plt.plot(t, mat_t[37])
-    # plt.subplot(122)
-    # plt.semilogx(f, mat_f[37])
+    # plt.contourf(contorno_x, contorno_y, mat_f, levels, cmap='jet', extend='min')
+    plt.contourf(contorno_x, contorno_y, mat_f, cmap='jet')
+    plt.xlim([0, 10000])
     plt.show()
 
 
 tiempo, impulsos, frecuencia, imp_fft = matriz_impulsos()
-print(type(imp_fft))
 
-grafico(tiempo, impulsos, frecuencia, imp_fft)
+suave = np.zeros([73, 513])
+for i in range(len(imp_fft)):
+    suave[i] = su.suavizado(frecuencia, imp_fft[i], 6)
 
+# plt.semilogx(frecuencia, suave[35], 'g')
+# plt.semilogx(frecuencia, suave[30], 'r')
+# plt.semilogx(frecuencia, suave[27], 'orange')
+# plt.semilogx(frecuencia, suave[23], 'yellow')
+# plt.semilogx(frecuencia, suave[36])
+# plt.xlim([240, 20000])
+# plt.show()
+
+grafico(frecuencia, imp_fft)
+# grafico(frecuencia, imp_fft)
+
+"""
+Ventaneo: 195 muestras
+R = 48000 HZ / 195 muestras = 246.15 Hz frec mínima
+"""
