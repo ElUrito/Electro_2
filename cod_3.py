640a7447762148cff8dd7067c25f7d96b107170a
Misley-tech <77426773+Misley-tech@users.noreply.github.com>
2021-11-05 19:10:00 -0300

    Update cod_3.py

--- cod_3.py
+++ cod_3.py
@@ -35,12 +35,13 @@ def mag_real(senal, orientacion='h', graficar='', guardar_img=''):
         plt.semilogx(f, senal_spl[i], label=labels[i])
         plt.xlim(250, 25000)
     ticksx = [250, 500, 1000, 2000, 4000, 8000, 16000]
-    plt.xticks(ticks=ticksx, labels=ticksx, rotation=0)
-    plt.ylabel(r'Nivel de presión sonora [dB SPL]')
-    plt.xlabel(r'Frecuencia [Hz]')
+    plt.xticks(ticks=ticksx, labels=ticksx, rotation=0, fontsize=20)
+    plt.ylabel(r'Nivel de presión sonora [dB SPL]',fontsize=20)
+    plt.xlabel(r'Frecuencia [Hz]',fontsize=20)
     plt.ylim(70, np.max(senal_spl) * 1.05)
     plt.grid(linewidth=1, which='both')
-    plt.legend(framealpha=1)
+    plt.legend(framealpha=1,fontsize=15,loc='lower left')
+    plt.yticks(fontsize=20)
     plt.tight_layout()
     if guardar_img == 'si' or guardar_img == 'SI' or guardar_img == 'Si':
         plt.savefig(f'Rta del sist {eje}')
@@ -48,44 +49,6 @@ def mag_real(senal, orientacion='h', graficar='', guardar_img=''):
         plt.show()
 
 
-def graf_polar(frecuencias, senal, orientacion='h', graficar='', guardar_img=''):
-    eje = 'horizontal'
-    if orientacion == 'v' or orientacion == 'V':
-        eje = 'vertical'
-
-    # Gráfico polar
-    grados = np.linspace(0, 180, 37, dtype=int)
-    fig, ax = plt.subplots(subplot_kw={'projection': 'polar'})
-    theta = np.zeros([73])
-    theta = np.pi * np.hstack((-1 * np.flip(grados), grados)) / 180
-    theta = np.delete(theta, 37)
-
-    freqs = [1000, 2000, 4000, 8000, 16000]
-    labels = ['1 kHz', '2 kHz', '4 kHz', '8 kHz', '16 kHz']
-
-    mat_polar = np.zeros([5, 73])
-    mat_polar[0] = senal[:, 23]
-    mat_polar[1] = senal[:, 43]
-    mat_polar[2] = senal[:, 86]
-    mat_polar[3] = senal[:, 171]
-    mat_polar[4] = senal[:, 342]
-    ax.plot(theta, mat_polar[0], label=labels[0])
-    ax.plot(theta, mat_polar[1], label=labels[1])
-    ax.plot(theta, mat_polar[2], label=labels[2])
-    ax.plot(theta, mat_polar[3], label=labels[3])
-    ax.plot(theta, mat_polar[4], label=labels[4])
-
-    ax.set_rlim((-55, 0))
-    ax.set_rticks(np.arange(-50, 0, 10))
-    ax.set_rlabel_position(0)
-    ax.tick_params(axis='x', which='both', labelsize=10)
-    ax.legend(bbox_to_anchor=(1.05, 1), loc='best')
-    ax.set_title(f'Patrón polar {eje}', fontsize=13, pad=15)
-    plt.tight_layout()
-    if guardar_img == 'si' or guardar_img == 'SI' or guardar_img == 'Si':
-        plt.savefig(f'polar_{eje}.png')
-    if graficar == 'si' or graficar == 'SI' or graficar == 'Si':
-        plt.show()
 
 
 # Ejecución de función
@@ -105,8 +68,8 @@ Se puede graficar y guardar la imagen en simultáneo de la siguiente manera:
 mag_real(senal, graficar='si', guardar_img='si')
 """
 _, senal, f, senal_fft = matriz_impulsos()
-# mag_real(senal, graficar='si')
-graf_polar(f, senal_fft, guardar_img='si')
+mag_real(senal, graficar='si')
+
 
 # _, senal2, f2, senal_fft2 = matriz_impulsos(orientacion='v')
-# graf_polar(f2, senal_fft2, guardar_img='si', orientacion='v')
+
